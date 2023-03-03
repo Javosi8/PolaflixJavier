488049e0a3d63377d8e696cf3a8a38cfe541e546
Javier Canales <javier.canales7.JC@gmail.com>
2023-03-03 18:16:44 +0100

    First commit

---
+++ polaflixjavier/src/main/java/es/unican/canalesj/empresariales/polaflixjavier/PolaflixjavierApplication.java
@@ -0,0 +1,13 @@
+package es.unican.canalesj.empresariales.polaflixjavier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaflixjavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaflixjavierApplication.class, args);
+	}
+
+}
---
+++ polaflixjavier/src/test/java/es/unican/canalesj/empresariales/polaflixjavier/PolaflixjavierApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.canalesj.empresariales.polaflixjavier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixjavierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
